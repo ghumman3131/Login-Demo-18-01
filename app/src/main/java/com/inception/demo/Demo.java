55a5e3bc96f5216f32d7c18788c6e9a7871b8536
ghumman3131 <charanghumman@gmail.com>
2018-01-18 19:07:16 +0530

    first commit

---
+++ app/src/main/java/com/inception/demo/Demo.java
@@ -0,0 +1,18 @@
+package com.inception.demo;
+
+/**
+ * Created by charanghumman on 17/01/18.
+ */
+
+public class Demo
+
+{
+
+    public Demo()
+    {
+
+    }
+
+
+
+}
--- app/src/main/java/com/inception/demo/FirstActivity.java
+++ app/src/main/java/com/inception/demo/FirstActivity.java
@@ -1,13 +1,50 @@
 package com.inception.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity
+
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_first);
+
     }
+
+    public void  doLogin(View v)
+    {
+
+        EditText username_et =  findViewById(R.id.et1);
+
+        EditText password_et =  findViewById(R.id.et2);
+
+        
+
+        if(username_et.getText().toString().equals("admin") && password_et.getText().toString().equals("123456"))
+        {
+            Intent i = new Intent(FirstActivity.this , SecondActivity.class);
+
+            startActivity(i);
+        }
+
+        else {
+
+            Toast.makeText(FirstActivity.this ,"invalid credentials",Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
 }
+
+
